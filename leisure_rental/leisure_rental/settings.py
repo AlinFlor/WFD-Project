c93fe383a1970f53f31621f25a84a7109e4f7f5a
AlinFlor <145985974+AlinFlor@users.noreply.github.com>
2025-04-06 12:47:12 +0100

    Update settings.py
    Custom User Model

--- leisure_rental/leisure_rental/settings.py
+++ leisure_rental/leisure_rental/settings.py
@@ -111,6 +111,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Custom user model
+AUTH_USER_MODEL = 'rental.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
