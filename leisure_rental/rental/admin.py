90bf6c0f6ef39bbf971c6eee70e7af9fdf7c37e8
AlinFlor <145985974+AlinFlor@users.noreply.github.com>
2025-04-06 12:52:44 +0100

    Register Users in Admin Panel

--- leisure_rental/rental/admin.py
+++ leisure_rental/rental/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Equipment, Rental, Invoice, InvoiceItem
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Equipment)
+admin.site.register(Rental)
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
