8985899c70d578a0ced7a26eb36d92b9a815129a
AlinFlor <145985974+AlinFlor@users.noreply.github.com>
2025-04-06 12:40:15 +0100

    Rental Class

--- leisure_rental/rental/models.py
+++ leisure_rental/rental/models.py
@@ -13,3 +13,16 @@ class Equipment(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
     daily_rate = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models. BooleanField(default=True)
+    condition = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+class Rental(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=20, default='Reserved')
+    
