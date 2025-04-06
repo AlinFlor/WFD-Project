f6da5d226c43482034dd6a8c58752566bfbe1c5d
AlinFlor <145985974+AlinFlor@users.noreply.github.com>
2025-04-06 12:49:50 +0100

    Troubleshooting

--- leisure_rental/rental/models.py
+++ leisure_rental/rental/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.authmodels import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 USER_ROLES = (
     ('CUSTOMER', 'Customer'),
