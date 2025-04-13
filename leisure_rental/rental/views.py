c1de6bd75bf3a013d4aace2947436e7979f41039
AlinFlor <145985974+AlinFlor@users.noreply.github.com>
2025-04-13 20:56:22 +0100

    .

--- leisure_rental/rental/views.py
+++ leisure_rental/rental/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Equipment
 
 def equipment_list(request):
-    equipments = Equipment.object.filter(available=True)
-    return rende(request, 'rental/equipment_list.html', {'equipments': equipments})
+    equipments = Equipment.objects.filter(available=True)
+    return render(request, 'rental/equipment_list.html', {'equipments': equipments})
